1280196522b655f3bc89ed0eaf922bd7cc121612
何祥君 <hxj1225@163.com>
2013-01-17 20:24:18 +0800

    init project

---
+++ src/main/java/com/hxj/simplejava/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2013 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.hxj.simplejava;
+
+/**
+ * 类Hello.java的实现描述：TODO 类实现描述 
+ * @author wb_xiangjun.hexj 2013-1-17 下午8:23:30
+ */
+public class Hello {
+
+}
